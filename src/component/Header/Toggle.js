864fe7997606002082d917538e779cfc2bf7e655
hannah <hannahhuang000@outlook.com>
2023-04-13 14:23:36 -0400

    update navbar

--- src/component/Header/Toggle.js
+++ src/component/Header/Toggle.js
@@ -1,23 +1,34 @@
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Container } from "react-bootstrap";
 import classes from "./Toggle.module.css";
 import { Fragment } from "react";
 
 const Toggle = (props) => {
   return (
-    <Fragment>
-      <ToggleButtonGroup
-        // value={alignment}
-        exclusive
-        // onChange={handleAlignment}
-        aria-label="text alignment"
-        className={classes.togglegroup}
-      >
-        <ToggleButton value="center" aria-label="left aligned">
-          {/* <Aboutme /> */}
-        </ToggleButton>
-      </ToggleButtonGroup>
-    </Fragment>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">HOME</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className={classes.togglegroup}>
+            <Nav.Link href="#features" className={classes.navlink}>
+              Experience
+            </Nav.Link>
+            <Nav.Link href="#projects" className={classes.navlink}>
+              Projects
+            </Nav.Link>
+            <Nav.Link href="#interests" className={classes.navlink}>
+              Interests
+            </Nav.Link>
+            <Nav.Link href="#contact" className={classes.navlink}>
+              Contact Me
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
